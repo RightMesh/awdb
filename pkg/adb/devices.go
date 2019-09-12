b08e55a3db3ad73b56157e88f3d1b471c4dba76f
Frazer Seymour <frazer@rightmesh.io>
2019-09-12 17:42:32 -0400

    Format JSON output.

--- pkg/adb/devices.go
+++ pkg/adb/devices.go
@@ -28,31 +28,31 @@ import (
 type Device struct {
 	// This device's unique ID on this host machine.
 	// e.g. 1
-	TransportID int
+	TransportID int `json:"transport_id"`
 
 	// The device's serial ID. Not guaranteed to be unique.
 	// e.g. "0a388e93"
-	SerialID string
+	SerialID string `json:"serial_id"`
 
 	// The device's state.
 	// One of "authorized", "unauthorized", "authorizing", "device", "offline", "no device".
-	State string
+	State string `json:"state"`
 
 	// The USB port the device is connected to.
 	// e.g. "1-1.4.2"
-	Usb string
+	Usb string `json:"usb"`
 
 	// Optional, the device's product name.
 	// e.g., "razor"
-	Product string
+	Product string `json:"product,omitempty"`
 
 	// Optional, the device's model name.
 	// e.g. "Nexus_7"
-	Model string
+	Model string `json:"model,omitempty"`
 
 	// Optional, the device's name.
 	// e.g. "flo"
-	Device string
+	Device string `json:"device,omitempty"`
 }
 
 // ParseDeviceList returns a slice of adb.Devices parsed from the output from `adb devices -l`.
